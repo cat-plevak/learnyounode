99861ff8766d5127aea1b564e0cd9f592aac9297
cat-plevak <catherine.plevak@colorado.edu>
2017-09-11 12:38:09 -0600

    first two

---
+++ program.js
@@ -0,0 +1,17 @@
+// console.log(process.argv);
+// let total = 0;
+//
+// function summed() {
+//   for(i = 2; i < process.argv.length; i++) {
+//     let number = Number(process.argv[i]);
+//     total += number;
+//   }
+//   console.log(total);
+// }
+//
+//
+// summed();
+
+
+var fs = require('fs');
+fs.readFileSync('/path/to/file');
